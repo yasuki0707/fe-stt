521774d69a57da8d383a92ae146d2f53538e890b
Yasuki Oshiro <Yasuki.Oshiro@ibm.com>
2022-02-05 20:15:47 +0900

    remove unnucessary files

--- pipeline/apply-tekton-catalogs.sh
+++
@@ -1,3 +0,0 @@
-kubectl apply -f https://raw.githubusercontent.com/tektoncd/catalog/main/task/npm/0.1/npm.yaml
-oc apply -f https://raw.githubusercontent.com/tektoncd/catalog/main/task/git-clone/0.3/git-clone.yaml
-oc apply -f https://raw.githubusercontent.com/tektoncd/catalog/main/task/buildah/0.1/buildah.yaml
--- pipeline/create-secret.sh
+++
@@ -1,8 +0,0 @@
-source .env
-
-kubectl create secret \
-    -n yasu \
-    docker-registry quay-io-stt-be-password \
-    --docker-server=$CONTAINER_REGISTRY_SERVER \
-    --docker-username=$CONTAINER_REGISTRY_USER \
-    --docker-password=$CONTAINER_REGISTRY_PASSWORD
