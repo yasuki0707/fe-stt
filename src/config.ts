8f0781b47556d55fe233276d11fb3aaac68e3cad
Yasuki Oshiro <Yasuki.Oshiro@ibm.com>
2022-02-07 11:03:40 +0900

    debug print

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,4 @@
+console.log('process.env.FE_PROFILE:', process.env.FE_PROFILE);
 export const API_HOST =
   process.env.FE_PROFILE === 'prod'
     ? 'https://route-be-yasu.itzroks-120000mck6-wefx4z-6ccd7f378ae819553d37d5f2ee142bd6-0000.eu-gb.containers.appdomain.cloud/'
