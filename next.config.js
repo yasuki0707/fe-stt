ff7a2fbfa6a4797a6fee99d552ecbd64d2e0dd9c
Yasuki Oshiro <Yasuki.Oshiro@ibm.com>
2022-02-07 12:11:37 +0900

    debug print

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 const withLinaria = require('next-linaria');
-
+console.log('process.env.FE_PROFILE:nextconfig', process.env.FE_PROFILE);
 module.exports = withLinaria({
   env: {
     FE_PROFILE: process.env.FE_PROFILE
