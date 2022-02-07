c9e37b8dbcfc3f0eee5f73e34bb7287a442b60d0
Yasuki Oshiro <Yasuki.Oshiro@ibm.com>
2022-02-07 10:35:40 +0900

    rename route

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 export const API_HOST =
   process.env.FE_PROFILE === 'prod'
-    ? 'https://yasu-route-yasu.itzroks-120000mck6-wefx4z-6ccd7f378ae819553d37d5f2ee142bd6-0000.eu-gb.containers.appdomain.cloud/'
+    ? 'https://route-be-yasu.itzroks-120000mck6-wefx4z-6ccd7f378ae819553d37d5f2ee142bd6-0000.eu-gb.containers.appdomain.cloud/'
     : 'http://localhost:3000'; // dev
